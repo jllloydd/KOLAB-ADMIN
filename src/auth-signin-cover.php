af1292762730b157a7cbb97f5c2749ba00d7ee9c
jlsh29 <jelbanaria@gmail.com>
2024-04-23 16:02:14 +0800

    Sign-In Page

--- src/auth-signin-cover.php
+++ src/auth-signin-cover.php
@@ -2,158 +2,63 @@
 <?php include 'layouts/head-main.php'; ?>
 
     <head>
-        
         <title>Sign In | Velzon - Admin & Dashboard Template</title>
         <?php include 'layouts/title-meta.php'; ?>
-
         <?php include 'layouts/head-css.php'; ?>
-
     </head>
 
     <?php include 'layouts/body.php'; ?>
-
-        <!-- auth-page wrapper -->
-        <div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
-            <div class="bg-overlay"></div>
-            <!-- auth-page content -->
-            <div class="auth-page-content overflow-hidden pt-lg-5">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card overflow-hidden">
-                                <div class="row g-0">
-                                    <div class="col-lg-6">
-                                        <div class="p-lg-5 p-4 auth-one-bg h-100">
-                                            <div class="bg-overlay"></div>
-                                            <div class="position-relative h-100 d-flex flex-column">
-                                                <div class="mb-4">
-                                                    <a href="index.php" class="d-block">
-                                                        <img src="assets/images/logo-light.png" alt="" height="18">
-                                                    </a>
-                                                </div>
-                                                <div class="mt-auto">
-                                                    <div class="mb-3">
-                                                        <i class="ri-double-quotes-l display-4 text-success"></i>
-                                                    </div>
-
-                                                    <div id="qoutescarouselIndicators" class="carousel slide" data-bs-ride="carousel">
-                                                        <div class="carousel-indicators">
-                                                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="0" class="active"
-                                                                aria-current="true" aria-label="Slide 1"></button>
-                                                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="1"
-                                                                aria-label="Slide 2"></button>
-                                                            <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="2"
-                                                                aria-label="Slide 3"></button>
-                                                        </div>
-                                                        <div class="carousel-inner text-center text-white-50 pb-5">
-                                                            <div class="carousel-item active">
-                                                                <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
-                                                            </div>
-                                                            <div class="carousel-item">
-                                                                <p class="fs-15 fst-italic">" The theme is really great with an amazing customer support."</p>
-                                                            </div>
-                                                            <div class="carousel-item">
-                                                                <p class="fs-15 fst-italic">" Great! Clean code, clean design, easy for customization. Thanks very much! "</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <!-- end carousel -->
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end col -->
-
-                                    <div class="col-lg-6">
-                                        <div class="p-lg-5 p-4">
-                                            <div>
-                                                <h5 class="text-primary">Welcome Back !</h5>
-                                                <p class="text-muted">Sign in to continue to Velzon.</p>
-                                            </div>
-            
-                                            <div class="mt-4">
-                                                <form action="index.php">
-                    
-                                                    <div class="mb-3">
-                                                        <label for="username" class="form-label">Username</label>
-                                                        <input type="text" class="form-control" id="username" placeholder="Enter username">
-                                                    </div>
-                            
-                                                    <div class="mb-3">
-                                                        <div class="float-end">
-                                                            <a href="auth-pass-reset-cover.php" class="text-muted">Forgot password?</a>
-                                                        </div>
-                                                        <label class="form-label" for="password-input">Password</label>
-                                                        <div class="position-relative auth-pass-inputgroup mb-3">
-                                                            <input type="password" class="form-control pe-5 password-input" placeholder="Enter password" id="password-input">
-                                                            <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
-                                                        </div>
-                                                    </div>
-                            
-                                                    <div class="form-check">
-                                                        <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
-                                                        <label class="form-check-label" for="auth-remember-check">Remember me</label>
-                                                    </div>
-                                                    
-                                                    <div class="mt-4">
-                                                        <button class="btn btn-success w-100" type="submit">Sign In</button>
-                                                    </div>
-                        
-                                                    <div class="mt-4 text-center">
-                                                        <div class="signin-other-title">
-                                                            <h5 class="fs-13 mb-4 title">Sign In with</h5>
-                                                        </div>
-        
-                                                        <div>
-                                                            <button type="button" class="btn btn-primary btn-icon waves-effect waves-light"><i class="ri-facebook-fill fs-16"></i></button>
-                                                            <button type="button" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-google-fill fs-16"></i></button>
-                                                            <button type="button" class="btn btn-dark btn-icon waves-effect waves-light"><i class="ri-github-fill fs-16"></i></button>
-                                                            <button type="button" class="btn btn-info btn-icon waves-effect waves-light"><i class="ri-twitter-fill fs-16"></i></button>
-                                                        </div>
-                                                    </div>
-                
-                                                </form>
-                                            </div>
-    
-                                            <div class="mt-5 text-center">
-                                                <p class="mb-0">Don't have an account ? <a href="auth-signup-cover.php" class="fw-semibold text-primary text-decoration-underline"> Signup</a> </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end col -->
-                                </div>
-                                <!-- end row -->
-                            </div>
-                            <!-- end card -->
+        <!-- <div>
+            <img src="assets/img/login/logo.png" width="half" class="img">
+        </div> -->
+        <div class="login_page p-0 g-0">
+            <div class="row bg-white">
+                <div class="col container-one">
+                    <div class="">
+                        <div class="main-container">
+                            <div class="logo-container">
+                                <img src="assets/img/login/logo.png" class="img2" alt="Logo">
+                            </div>                            
+                            <img src="assets/img/login/left.png" width="half" class="img" alt="Left Image">
                         </div>
-                        <!-- end col -->
-    
                     </div>
-                    <!-- end row -->
                 </div>
-                <!-- end container -->
+                <!-- end col -->
+
+                <div class="col main-container container d-flex justify-content-center align-items-center">
+                    <d class="sub-container" style="background-color: #FAFAFA;">
+                        <h2 class="pt-5 ps-5 fw-bold" style="color: #0683D7;">ADMIN</h2>
+                        <div class="ps-5 pe-5 pt-3">
+                            <div class="mb-3">
+                                <label class="chromaphobic fw-medium">Username</label><br>
+                                <input type="username" name="username" class="form-control bg-white" style="color: #8D97B0">
+                            </div>
+                            <div class="mb-2">
+                                <label class="chromaphobic fw-medium">Password</label><br>
+                                <input type="password" name="password" class="form-control bg-white" style="color: #8D97B0">
+                            </div>
+                            <div>
+                                <p class="mt-3">Please contact admin to change your credentials.</p>
+                            </div>
+
+                            <div class="container">
+                            </div>
+                            <div class="row p-0 mt-5">
+                                <label class="chromaphobic"></label>
+                                <span>
+                                    <button id="checkBookingStatus" class="next-button-position mb-5 p-1" type="button">
+                                        <span class="btn next-button" id="checkBookingStatus">PROCEED</span>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end col -->
             </div>
-            <!-- end auth page content -->
-
-            <!-- footer -->
-            <footer class="footer">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="text-center">
-                                <p class="mb-0">&copy; <script>document.write(new Date().getFullYear())</script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-            <!-- end Footer -->
         </div>
-        <!-- end auth-page-wrapper -->
-
         <?php include 'layouts/vendor-scripts.php'; ?>
 
-        <!-- password-addon init -->
         <script src="assets/js/pages/password-addon.init.js"></script>
     </body>
 
