1c075d3ccd551aad85cd9f54d25d77e78f6d826d
Breiana Cirilo <breianacirilo@gmail.com>
2024-04-23 10:29:05 +0800

    updated index

--- src/layouts/head-css.php
+++ src/layouts/head-css.php
@@ -1,10 +1,11 @@
 <!-- Layout config Js -->
 <script src="assets/js/layout.js"></script>
+<!-- custom Css-->
+<link href="assets/css/custom.css" id="app-style" rel="stylesheet" type="text/css" />
 <!-- Bootstrap Css -->
 <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
 <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 <!-- App Css-->
 <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
-<!-- custom Css-->
-<link href="assets/css/custom.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
