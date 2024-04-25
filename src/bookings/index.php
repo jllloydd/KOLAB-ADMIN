fee81b0120cb0b6df3b55f404353b8955ee08712
ricesough <106121957+ricesough@users.noreply.github.com>
2024-04-25 11:12:18 +0800

    removed extra pagination

--- src/bookings/index.php
+++ src/bookings/index.php
@@ -284,31 +284,7 @@
                                                 orders for you search.</p>
                                         </div>
                                     </div>
-                                </div>
-
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <p>Showing 5 from 46 data</p>
-                                    <nav aria-label="Page navigation example">
-                                        <ul class="pagination">
-                                            <li class="page-item">
-                                            <a class="page-link" href="#" aria-label="Previous">
-                                                <span aria-hidden="true">&laquo;</span>
-                                            </a>
-                                            </li>
-                                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                            <li class="page-item"><a class="page-link" href="#">4</a></li>
-                                            <li class="page-item">
-                                            <a class="page-link" href="#" aria-label="Next">
-                                                <span aria-hidden="true">&raquo;</span>
-                                            </a>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                </div>
-
-                                
+                                </div>                                
                             </div>
                         </div>
                         <div class="d-flex justify-content-between align-items-center">
