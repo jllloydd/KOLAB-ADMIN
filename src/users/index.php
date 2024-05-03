57a6f8ceeeb06ee32fa364cca4a002667de4ada2
jlsh29 <jelbanaria@gmail.com>
2024-05-03 16:24:49 +0800

    Updated Users-Modal

--- src/users/index.php
+++ src/users/index.php
@@ -105,20 +105,133 @@
             
 
             <div>
-                <button type="button" class="btn btn-info " data-bs-toggle="modal" data-bs-target=".bs-example-modal-xl">Extra large Modal</button>
-                    <div class="modal fade bs-example-modal-xl mt-5" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div>
-                                    <div>
-                                        <h4 class="bg-info">Julienne Galvez</h4>
+                <button type="button" class="btn btn-info " data-bs-toggle="modal" data-bs-target=".bs-example-modal-xl">View</button>
+                
+                <div class="modal fade bs-example-modal-xl mt-5" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl">
+                        <div class="modal-content main-user-container container">
+                            <!--Profile Page-->
+                            <div class="first-user-container bg-white me-3">
+
+                                <div class="title-user">
+                                    <h5 class="user-name fw-meduim pb-2">Julienne Galvez</h5>
+                                    <div class="users-profile">
+                                        <div><img src="../assets/img/users/diamond.svg" class="user-name me-0"></div>
+                                        <div><p class="user-name fw-medium">Maharlika</p></div>
                                     </div>
                                 </div>
-                                <div></div>
-                                <div></div>
+
+                                <div class="container">
+                                    <div>
+                                        <div class="personal-info mt-4 ms-3 mb-4">
+                                            <div class="me-2"><img src="../assets/img/users/phone.svg" class="user-name me-0" width="18px"></div>
+                                            <div><p class="user-name mt-1 fw-normal"><span>+63 </span>9876543210</p></div>
+                                        </div>
+                                        <div class="personal-info mt-2 ms-3 mb-4">
+                                            <div class="me-2"><img src="../assets/img/users/email.svg" class="user-name me-0" width="18px"></div>
+                                            <div><p class="user-name mt-1 fw-normal">juliennegalvez@gmail.com</p></div>
+                                        </div>
+                                        <div class="personal-info mt-2 ms-3 mb-4">
+                                            <div class="me-2"><img src="../assets/img/users/bday.svg" class="user-name me-0" width="18px"></div>
+                                            <div><p class="user-name mt-1 fw-normal">January 1, 2000</p></div>
+                                        </div>
+                                        <div class="personal-info mt-2 ms-3 mb-4">
+                                            <div class="me-2"><img src="../assets/img/users/address.svg" class="user-name me-0" width="18px"></div>
+                                            <div class="address-container"><p class="user-name mt-1 fw-normal">128 Dominican Rd., Lourdes Proper, Baguio City</p></div>
+                                        </div>
+                                        <div class="personal-info mt-2 ms-3 mb-4">
+                                            <div class="me-2"><img src="../assets/img/users/status.svg" class="user-name me-0" width="18px"></div>
+                                            <div><p class="user-name mt-1 fw-normal">Student</p></div>
+                                        </div>
+                                        <div class="personal-info mt-2 ms-3 mb-4">
+                                            <div class="me-2"><img src="../assets/img/users/school.svg" class="user-name me-0" width="18px"></div>
+                                            <div><p class="user-name mt-1 fw-normal">University of the Cordilleras</p></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--Inclusions-->
+                            <div class="container second-user-container bg-white pt-5 ps-4 me-3">
+                                <div class="">
+                                    <h5 class="fw-light mb-4 inclusion-title">Inclusions</h5>
+                                </div>
+                                <div class="">
+                                    <ul class="list-inclusions">
+                                        <li class="mb-2">Virtual Office Address</li>
+                                        <li class="mb-2">Host Events 2x</li>
+                                        <li class="mb-2">Member Announcement on BLINC FB Page</li>
+                                        <li class="mb-2">Logo Placement on Website industry partner list and social media</li>
+                                        <li class="mb-2">Member Feature on BLINC Magazines</li>
+                                        <li class="mb-2">Use of BLINC logo</li>
+                                        <li class="mb-2">Certificate of Recognition</li>
+                                        <li class="mb-2">RSVP events invitation via email</li>
+                                        <li class="mb-2">Participation discounts for BLINC events</li>
+                                        <li class="mb-2">Networking Opportunities</li>
+                                        <li class="mb-2">Inclusive of Collab Amenities</li>
+                                        <li class="mb-2">Discounts on other BLINC Services</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <!--Billings-->
+                            <div class="container third-user-container bg-white pt-5 ps-4">
+
+                                <div class="row mb-5">
+                                    <div class="billing-list" >
+                                        <div class="col d-flex flex-column">
+                                            <h5 class="fw-normal me-auto guest-style">Billing and Payment</h5>
+                                        </div>
+                                        <div class="align-content-center justify-content-center">
+                                            <a href="#" class="billing-history">View billing history</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mb-4">
+                                    <div class="billing-list" >
+                                        <div class="col d-flex flex-column">
+                                            <h5 class="me-auto guest-style">Online</h5>
+                                        </div>
+                                        <div class="align-content-center justify-content-center">
+                                            <p class="running-late ps-2 pe-2 pt-1 pb-1">Pending</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mb-5">
+                                    <div class="billing-list" >
+                                        <div class="col d-flex flex-column">
+                                            <div class="billing-payment">
+                                                <div><img src="../assets/img/users/diamond.svg" class="user-name me-0"></div>
+                                                <div><h5 class="user-name fw-normal">Maharlika</h5></div>
+                                            </div>
+                                        </div>
+                                        <div class="align-content-center justify-content-center">
+                                            <h5 class="fw-normal">₱86,000/year</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mb-5">
+                                    <div class="billing-list" >
+                                        <div class="col d-flex flex-column">
+                                            <h5 class="fw-normal me-auto guest-style">Next payment due on</h5>
+                                        </div>
+                                        <div class="align-content-center justify-content-center">
+                                            <h5 class="fw-normal">April 1, 2025</h5>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="button-billing">
+                                    <div><button class="hold-billing">HOLD</button></div>
+                                    <div><button class="cancel-billing">CANCEL</button></div>
+                                </div>
+
+                                    
+                                
                             </div>
                         </div>
                     </div>
+                </div>
             </div>
 
 
