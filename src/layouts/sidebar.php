45f6b14a8d8eb6624c0931b92b9e72687077ab5f
Breiana Cirilo <breianacirilo@gmail.com>
2024-05-08 09:55:44 +0800

    fixed sidebar and topbar

--- src/layouts/sidebar.php
+++ src/layouts/sidebar.php
@@ -30,31 +30,28 @@
 
             <div id="scrollbar">
                 <div class="container-fluid">
-
-                    <div id="two-column-menu">
-                    </div>
-                    <ul class="navbar-nav" id="navbar-nav">
+                    <ul class="navbar-nav d-flex" id="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link menu-link d-flex flex-column" href="../dashboard/">
-                                <div class="image-dashboard"></div>
+                            <a class="nav-link menu-link d-flex flex-column align-items-center" href="../dashboard/">
+                                <div class="image-dashboard topbar-icon"></div>
                                 <span class="dashboard-text"><?=$lang['t-dashboard']?></span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link menu-link d-flex flex-column" href="../bookings/">
-                                <div class="image-booking"></div>
+                            <a class="nav-link menu-link d-flex flex-column align-items-center" href="../bookings/">
+                                <div class="image-booking topbar-icon"></div>
                                 <span class="booking-text"><?=$lang['t-bookings']?></span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link menu-link d-flex flex-column" href="../admins/">
-                                <div class="image-admin"></div>
+                            <a class="nav-link menu-link d-flex flex-column align-items-center" href="../admins/">
+                                <div class="image-admin topbar-icon"></div>
                                 <span class="admin-text"><?=$lang['t-admins']?></span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link menu-link d-flex flex-column" href="../users/">
-                                <div class="image-user"></div>
+                            <a class="nav-link menu-link d-flex flex-column align-items-center" href="../users/">
+                                <div class="image-user topbar-icon"></div>
                                 <span class="user-text"><?=$lang['t-users']?></span>
                             </a>
                         </li>
@@ -65,12 +62,11 @@
             </div>
             <div class="sidebar-background"></div>
         </div>
-
-        <div class="sidebar-background"></div>
     </div>
 </div>
 <script src="../assets/js/sidebar.js"></script>
 <!-- Left Sidebar End -->
 <!-- Vertical Overlay-->
 <div class="vertical-overlay"></div>
+
 
